97c1c9909550e15b9fe57dd71add9160ad49866c
Michael Rodas <mrodas@bol.com>
2023-03-10 18:24:43 +0100

    Upgrade SpringBoot and Gradle libraries

--- src/main/java/com/cm/businessdirectory/gateway/web/BusinessController.java
+++ src/main/java/com/cm/businessdirectory/gateway/web/BusinessController.java
@@ -4,6 +4,7 @@ import com.cm.businessdirectory.usecase.CreateBusiness;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -12,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
